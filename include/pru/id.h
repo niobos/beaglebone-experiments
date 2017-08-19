6d2cf8eb28f892ed06480e9b9bda45b840954d20
Niobos <niels@dest-unreach.be>
2017-08-19 20:43:53 +0200

    Add PRU-ID header to get current PRU number

---
+++ include/pru/id.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if defined __AM335X_PRU0__
+#define PRU_NUMBER 0
+#define PRU_NUMBER_STR "0"
+#elif defined __AM335X_PRU1__
+#define PRU_NUMBER 1
+#define PRU_NUMBER_STR "1"
+#endif
